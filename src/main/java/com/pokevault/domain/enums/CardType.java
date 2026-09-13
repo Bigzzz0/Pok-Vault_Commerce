98baa35b2fd5043d2b5b604be34a900b3fb62fe7
sikarin <sikarin.u@kkumail.com>
2026-09-13 21:14:16 +0700

    feat(domain): define CardType, Rarity and ElementType enums

--- src/main/java/com/pokevault/domain/enums/CardType.java
+++ src/main/java/com/pokevault/domain/enums/CardType.java
@@ -1,4 +1,7 @@
 package com.pokevault.domain.enums;
 
 public enum CardType {
+    POKEMON,
+    TRAINER_SUPPORTER,
+    TRAINER_ITEM
 }
--- src/main/java/com/pokevault/domain/enums/ElementType.java
+++ src/main/java/com/pokevault/domain/enums/ElementType.java
@@ -1,4 +1,13 @@
 package com.pokevault.domain.enums;
 
 public enum ElementType {
+    GRASS,
+    FIRE,
+    WATER,
+    LIGHTNING,
+    PSYCHIC,
+    FIGHTING,
+    DARKNESS,
+    METAL,
+    COLORLESS
 }
