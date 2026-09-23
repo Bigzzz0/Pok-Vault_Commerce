36bcfce4928ed99bef29d864849c6528c0b58ecc
TanapoomChanthra <tanapoom.c@kkumail.com>
2026-09-23 18:27:52 +0700

    feat: create OrderRepository and OrderItemRepository

--- src/main/java/com/pokevault/repository/OrderItemRepository.java
+++ src/main/java/com/pokevault/repository/OrderItemRepository.java
@@ -1,4 +1,15 @@
 package com.pokevault.repository;
 
-public interface OrderItemRepository {
+import com.pokevault.domain.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    List<OrderItem> findByOrderId(Long orderId);
+
+    List<OrderItem> findByAssignedAccountId(Long accountId);
 }
