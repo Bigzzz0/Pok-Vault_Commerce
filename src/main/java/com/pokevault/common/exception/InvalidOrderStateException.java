2d9cc9846cc945f1cc563eedbffcd54b036f8b92
Tankun Phannikul <68142176+tankeiei@users.noreply.github.com>
2026-09-24 00:35:00 +0700

    feat: create custom exceptions for stock and invalid order states

--- src/main/java/com/pokevault/common/exception/InvalidOrderStateException.java
+++ src/main/java/com/pokevault/common/exception/InvalidOrderStateException.java
@@ -1,4 +1,12 @@
 package com.pokevault.common.exception;
 
-public class InvalidOrderStateException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/com/pokevault/common/exception/ResourceNotFoundException.java
+++ src/main/java/com/pokevault/common/exception/ResourceNotFoundException.java
@@ -1,4 +1,12 @@
 package com.pokevault.common.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
