1231d23a788828cfecb61e061dc4c84e81e42369
TanapoomChanthra <tanapoom.c@kkumail.com>
2026-09-25 11:44:15 +0700

    feat: create OrderPlacedEvent class for Observer event payload

--- src/main/java/com/pokevault/modules/order/event/OrderPlacedEvent.java
+++ src/main/java/com/pokevault/modules/order/event/OrderPlacedEvent.java
@@ -1,4 +1,24 @@
 package com.pokevault.modules.order.event;
 
+import com.pokevault.domain.entity.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderPlacedEvent {
+
+    private Long orderId;
+    private String orderCode;
+    private List<OrderItem> items;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
 }
