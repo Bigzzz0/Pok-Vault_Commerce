30bb458406476d30ca3c5e025b7ef393d9325688
TanapoomChanthra <tanapoom.c@kkumail.com>
2026-09-26 17:20:53 +0700

    fix: align InsufficientStockException constructors with develop

--- src/main/java/com/pokevault/common/exception/InsufficientStockException.java
+++ src/main/java/com/pokevault/common/exception/InsufficientStockException.java
@@ -9,4 +9,9 @@ public class InsufficientStockException extends RuntimeException {
     public InsufficientStockException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public InsufficientStockException(String cardName, int requestedQty, int availableQty) {
+        super(String.format("Insufficient stock for card '%s'. Requested: %d, Available: %d", 
+                cardName, requestedQty, availableQty));
+    }
 }
