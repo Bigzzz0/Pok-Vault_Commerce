9b8d72e24b5f501c26c4d2083bdb025d7efa9d67
Tankun Phannikul <68142176+tankeiei@users.noreply.github.com>
2026-09-26 15:57:16 +0700

    feat: implement OrderContext to manage current order state

--- src/main/java/com/pokevault/modules/trade/state/OrderContext.java
+++ src/main/java/com/pokevault/modules/trade/state/OrderContext.java
@@ -1,4 +1,66 @@
 package com.pokevault.modules.trade.state;
 
+import com.pokevault.domain.entity.Order;
+import com.pokevault.domain.enums.OrderStatus;
+
 public class OrderContext {
+
+    private Order order;
+    private OrderState currentState;
+
+    public OrderContext() {
+    }
+
+    public OrderContext(OrderState initialState) {
+        this.currentState = initialState;
+    }
+
+    public OrderContext(Order order, OrderState initialState) {
+        this.order = order;
+        this.currentState = initialState;
+    }
+
+    public void setState(OrderState state) {
+        this.currentState = state;
+    }
+
+    public OrderState getCurrentState() {
+        return currentState;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public OrderStatus getStatus() {
+        return currentState != null ? currentState.getStatus() : null;
+    }
+
+    public void pay() {
+        if (currentState != null) {
+            currentState.pay(this);
+        }
+    }
+
+    public void ship() {
+        if (currentState != null) {
+            currentState.ship(this);
+        }
+    }
+
+    public void complete() {
+        if (currentState != null) {
+            currentState.complete(this);
+        }
+    }
+
+    public void cancel() {
+        if (currentState != null) {
+            currentState.cancel(this);
+        }
+    }
 }
